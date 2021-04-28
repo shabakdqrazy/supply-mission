8ea737081fd0e91c7c3190e7b30642f96e87f134
shabakdqrazy <74818339+shabakdqrazy@users.noreply.github.com>
2021-04-28 21:06:39 +0530

    Add files via upload

---
+++ log.js
@@ -0,0 +1,25 @@
+class Log {
+    constructor(x, y,width,height) {
+      var options = {
+         isStatic:true
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      rotate(angle);
+      translate(pos.x, pos.y);
+      rectMode(CENTER);
+      strokeWeight(4);
+      stroke("red");
+      
+      fill(255);
+      rect(0, 0, this.width, this.height);
+      pop();
+    }
+  };
